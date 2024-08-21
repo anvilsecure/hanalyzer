c3bf06f751900cacde0f6afb33d61ba597c9c052
Alex Conti <alex.conti@anvilsecure.com>
2024-08-21 18:01:33 +0200

    modified default folder name

--- cmd/helper.go
+++ cmd/helper.go
@@ -24,7 +24,7 @@ func prepareOutputFolder() error {
 	if err != nil {
 		return fmt.Errorf("reading CWD: %s", err.Error())
 	}
-	outputPath = filepath.Join(cwd, fmt.Sprintf("%s_hana_output", time.Now().Format("20060102150405")))
+	outputPath = filepath.Join(cwd, fmt.Sprintf("%s_hana_output", time.Now().Format("20060102_150405")))
 	err = os.MkdirAll(outputPath, os.ModePerm)
 	if err != nil {
 		return fmt.Errorf("creating folder '%s': %s", outputPath, err.Error())
