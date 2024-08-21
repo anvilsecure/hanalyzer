446ad4b914bece077d1cc90a6c6b8887b3701ab1
Alex Conti <alex.conti@anvilsecure.com>
2024-08-21 15:47:31 +0200

    add DumpFiles to JSON output

--- checks/evaluateResults.go
+++ checks/evaluateResults.go
@@ -801,19 +801,52 @@ func EvaluateResults(checkType CheckType) {
 					check.IssuesPresent = false
 					check.Out = "[+] Trace files not found\n"
 				}
-			case "DumpFiles": // output: todo
+			case "DumpFiles": // output: DONE
+				var affectedResources = []struct {
+					FileName  string `json:"FileName"`
+					FileSize  int64  `json:"FileSize"`
+					FileMTime string `json:"FileMTime"`
+				}{}
 				if len(check.Results) > 0 {
-					utils.Warning("[!] Dump files found.\n")
-					for _, f := range check.Results {
+					message = "[!] Dump files found.\n"
+					check.IssuesPresent = true
+					for _, file := range check.Results {
+						fileSize, ok := file["FILE_SIZE"].(int64)
+						if !ok {
+							logger.Log.Errorf("Error during assertion of fileSize '%s' to string", file["FILE_SIZE"])
+						}
+						fileMTime, ok := file["FILE_MTIME"].(string)
+						if !ok {
+							logger.Log.Errorf("Error during assertion of fileMTime '%s' to string", file["FILE_MTIME"])
+						}
+						fileName, ok := file["FILE_NAME"].(string)
+						if !ok {
+							logger.Log.Errorf("Error during assertion of fileName '%s' to string", file["FILE_NAME"])
+						}
+						affectedResources = append(affectedResources, struct {
+							FileName  string "json:\"FileName\""
+							FileSize  int64  "json:\"FileSize\""
+							FileMTime string "json:\"FileMTime\""
+						}{
+							FileName:  fileName,
+							FileSize:  fileSize,
+							FileMTime: fileMTime,
+						})
+					}
+					var resourcesAsInterface []interface{}
+					for _, resource := range affectedResources {
+						resourcesAsInterface = append(resourcesAsInterface, resource)
 						utils.Info(fmt.Sprintf(
 							"%-20s %-20s %-s\n",
-							fmt.Sprintf("%-10d byte", f["FILE_SIZE"]),
-							f["FILE_MTIME"],
-							f["FILE_NAME"],
+							fmt.Sprintf("%-10d byte", resource.FileSize),
+							resource.FileMTime,
+							resource.FileName,
 						))
 					}
+					check.AffectedResources = resourcesAsInterface
 				} else {
-					utils.Ok("[+] Dump files not found\n")
+					check.IssuesPresent = false
+					check.Out = "[+] Dump files not found\n"
 				}
 			case "SAMLBasedAuthN": // output: todo
 				if len(check.Results) > 0 {
