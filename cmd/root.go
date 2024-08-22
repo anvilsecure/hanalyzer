34650ce1252e5ce27eb8aaed2f86a5cbbb59f71e
Alex Conti <alex.conti@anvilsecure.com>
2024-08-22 11:23:14 +0200

    removed defaultJSONOutput variable, added outputFolder variable

--- cmd/root.go
+++ cmd/root.go
@@ -8,17 +8,17 @@ import (
 )
 
 var (
-	configFile        string
-	host              string
-	SID               string
-	dbPort            int
-	dbUsername        string
-	dbPassword        string
-	sshUsername       string
-	sshPassword       string
-	sshPort           int
-	defaultJSONOutput string
-	jsonOutput        string
+	configFile   string
+	host         string
+	SID          string
+	dbPort       int
+	dbUsername   string
+	dbPassword   string
+	sshUsername  string
+	sshPassword  string
+	sshPort      int
+	jsonOutput   string
+	outputFolder string
 )
 
 var rootCmd = &cobra.Command{
