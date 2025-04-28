a0bbce0a2fdbf019dd396f30e3c6f467db293c42
Alex Conti <alex.conti@anvilsecure.com>
2025-04-28 17:58:21 +0200

    add flags and parsing logic for SSH public key authentication

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -41,19 +41,37 @@ var sshCmd = &cobra.Command{
 				log.Fatalf("error during configuration loading: %s\n", err.Error())
 			}
 		} else {
+			// if --priv-key is passed, take that value, otherwis it'll be nil
+			config.Conf.SSH.PrivateKey = sshPrivKey
+			// read SSH PASSWORD from env variable
 			sshPassword = os.Getenv("HANA_SSH_PASSWORD")
-			if sshPassword == "" {
-				logger.Log.Error("Environment variable HANA_SSH_PASSWORD is empty or not set.")
-				logger.Log.Info("Please provide the SSH password by setting it:\nexport HANA_SSH_PASSWORD=myverysecretpassword")
+			// if SSH PASSWORD is empty and also the password for the priv key
+			// then exit, otherwise one of them is used
+			if sshPassword == "" && sshPrivKey == "" {
+				logger.Log.Error("Environment variable HANA_SSH_PASSWORD is empty or not set and no private key was provided.")
+				logger.Log.Info("Either provide a private key or set the environment variable HANA_SSH_PASSWORD by setting it:\nexport HANA_SSH_PASSWORD=myverysecretpassword")
 				os.Exit(1)
+			}
+			// SSH private key is provided
+			if sshPrivKey != "" {
+				// read SSH priv key password from env variable
+				sshPrivKeyPass := os.Getenv("HANA_SSH_PRIV_KEY_PASSWORD")
+				// if it's empty, then exit
+				/*if sshPrivKeyPass == "" {
+					logger.Log.Error("Environment variable HANA_SSH_PRIV_KEY_PASSWORD is empty or not set")
+					logger.Log.Info("Provide a private key password by setting it:\nexport HANA_SSH_PRIV_KEY_PASSWORD=myverysecretpassword")
+					os.Exit(1)
+				}*/
+				// otherwise set it as priv key password in the SSH config
+				config.Conf.SSH.PrivateKeyPassword = sshPrivKeyPass
+			} else { // SSH private key is not provided, then the password must have been provided
+				config.Conf.SSH.Password = sshPassword
 			}
 			config.Conf.Host = host
 			config.Conf.SSH.Port = sshPort
 			config.Conf.SSH.Username = sshUsername
-			config.Conf.SSH.Password = sshPassword
 			config.Conf.SSH.IgnoreHostKey = sshIgnoreHostKey
 		}
-
 		ssh.Config()
 		checks.CURRENT_CHECK_TYPE = checkType.String()
 		checks.CreateChecks(checkType)
@@ -95,5 +113,6 @@ func init() {
 	sshCmd.Flags().StringVar(&sshUsername, "ssh-username", "", "SSH username")
 	sshCmd.Flags().StringVar(&outputFolder, "output-folder", "", "Output folder")
 	sshCmd.Flags().BoolVar(&sshIgnoreHostKey, "ignore-host-key", false, "Ignore host key error")
+	sshCmd.Flags().StringVar(&sshPrivKey, "priv-key", "", "SSH private key")
 	rootCmd.AddCommand(sshCmd)
 }
